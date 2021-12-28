5c13c62cb44449449ec597389b6800180ba52302
Arslan Ali <arslanali.921996@gmail.com>
2021-12-28 21:48:31 +0500

    add task template

--- src/components/KanbanTaskList.vue
+++ src/components/KanbanTaskList.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="task-list card rounded-2 shadow-1 border-0">
     <div class="task-list__body card-body rounded-2 p-2">
-      <div class="task-list__body__title card-title d-flex justify-content-between align-items-center">
+      <div
+        class="task-list__body__title card-title d-flex justify-content-between align-items-center"
+      >
         <div class="task-list__title">
-         <input type="text" class="form-control  form-control-sm" value="some list">
-        </div>
-        <div class="task-list__menu">
-          <i
-            class="bi bi-three-dots"
-            role="button"
+          <input
+            type="text"
+            class="form-control form-control-sm"
+            value="some list"
           />
         </div>
+        <div class="task-list__menu">
+          <i class="bi bi-three-dots" role="button" />
+        </div>
       </div>
-      <div class="task-list__tasks" />
+      <div class="task-list__tasks mb-2">
+        <task-card
+          :task="{
+            title: 'some task',
+            description: 2,
+            comments: 2,
+            subscribe: 2,
+            attachments: 2,
+            members: 2,
+          }"
+        />
+      </div>
       <div class="task-list__add">
         <list-input />
       </div>
@@ -23,29 +37,31 @@
 <script>
 import { defineComponent } from 'vue'
 import ListInput from '@/components/ListInput.vue'
+import TaskCard from '@/components/TaskCard.vue'
 export default defineComponent({
   components: {
-    ListInput
+    ListInput,
+    TaskCard
   }
 })
 </script>
 
 <style lang="scss" scoped>
-  .task-list {
+.task-list {
+  background: #ebecf0;
+  &__body {
+    max-width: 280px;
     background: #ebecf0;
-    &__body {
-      max-width: 280px;
-      background: #ebecf0;
-      &__title {
-        input {
-          font-weight: 500;
-          background: transparent;
-          border: none;
-          &:focus {
-            box-shadow: inset 0 0 0 2px #0079bf;
-          }
+    &__title {
+      input {
+        font-weight: 500;
+        background: transparent;
+        border: none;
+        &:focus {
+          box-shadow: inset 0 0 0 2px #0079bf;
         }
       }
     }
   }
+}
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="task-card card rounded-2" role="button">
+    <div class="task-card__cover">
+      <img src="@/assets/images/board.png" class="img-fluid" alt="board" />
+    </div>
+    <div
+      class="task-card__body card-body rounded-2 py-1 px-2"
+      @mouseover="showEditIcon = true"
+      @mouseout="showEditIcon = false"
+    >
+      <i
+        v-show="showEditIcon"
+        class="bi bi-pencil task-card__body__editicon"
+      ></i>
+      <div
+        class="task-card__body__labels d-flex flex-wrap gap-2 justify-content-start align-items-center mb-1"
+      >
+        <span class="bg-primary px-4 py-2 rounded-2"></span>
+        <span class="bg-danger px-4 py-2 rounded-2"></span>
+        <span class="bg-warning px-4 py-2 rounded-2"></span>
+        <span class="bg-warning px-4 py-2 rounded-2"></span>
+        <span class="bg-warning px-4 py-2 rounded-2"></span>
+      </div>
+      <div
+        class="task-card__body__title card-title d-flex justify-content-between align-items-center mb-1"
+      >
+        <span> {{ task.title }} </span>
+      </div>
+      <div
+        class="task-card__body__actions d-flex flex-wrap gap-3 justify-content-start mb-1"
+      >
+        <div
+          class="task-card__body__actions__subscribe d-flex gap-1 justify-content-start align-items-center"
+        >
+          <i class="bi bi-eye"></i>
+        </div>
+        <div
+          class="task-card__body__actions__description d-flex gap-1 justify-content-start align-items-center"
+        >
+          <i class="bi bi-justify-left"></i>
+        </div>
+        <div
+          class="task-card__body__actions__comments d-flex gap-1 justify-content-start align-items-center"
+        >
+          <i class="bi bi-chat"></i>
+        </div>
+        <div
+          class="task-card__body__actions__attachments d-flex gap-1 justify-content-start align-items-center flex-grow-1"
+        >
+          <i class="bi bi-paperclip"></i>
+          <span>
+            {{ task.attachments }}
+          </span>
+        </div>
+        <div
+          class="task-card__body__actions__members d-flex gap-1 justify-content-start align-items-center"
+        >
+          <!-- members avatars -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      showEditIcon: false
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.task-card {
+  &__body {
+    position: relative;
+    background: #ffffff;
+    box-shadow: 0 1px 0 #091e4240;
+    font-weight: 500;
+    &:hover {
+      background: #f4f5f7;
+    }
+    &__title {
+      font-size: 14px;
+      color: #172b4d;
+    }
+    &__editicon {
+      position: absolute;
+      right: 8px;
+      top: 8px;
+      font-size: 14px;
+      color: #6b778c;
+      z-index: 1;
+      padding-left: 6px;
+      padding-right: 6px;
+      padding-top: 2px;
+      padding-bottom: 2px;
+      border-radius: 3px;
+      &:hover {
+        background: #091e4214;
+      }
+    }
+    &__actions {
+      font-size: 14px;
+      color: #6b778c;
+      i {
+        font-size: 14px;
+        font-weight: 700;
+      }
+    }
+  }
+}
+</style>
