e7e36e48293c3743fc6cd83ddd9b523cba58ae41
Arslan Ali <arslanali.921996@gmail.com>
2022-02-21 21:09:10 +0500

    add vuex to app

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import axios from './plugins/axios'
 import toast from './plugins/toast'
+import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 const app = createApp(App)
 app.config.unwrapInjectedRef = true
-app.use(router).use(axios).use(toast).mount('#app')
+app.use(store).use(router).use(axios).use(toast).mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { BoardModel } from '@/types/entities'
-import { reactive } from 'vue'
+import { createStore } from 'vuex'
 
-export interface MyStore {
+interface MyStore {
   workspace: {
     createdBoards: Array<BoardModel>
     invitedBoards: Array<BoardModel>
@@ -9,19 +9,37 @@ export interface MyStore {
   currentBoard: BoardModel
 }
 
-const store: MyStore = reactive({
-  workspace: {
-    createdBoards: [],
-    invitedBoards: []
+const store = createStore({
+  state (): MyStore {
+    return {
+      workspace: {
+        createdBoards: [],
+        invitedBoards: []
+      },
+      currentBoard: { _id: '', name: ' ', createdAt: '', updatedAt: '' }
+    }
   },
-  currentBoard: {
-    _id: '',
-    name: '',
-    createdAt: '',
-    updatedAt: ''
-  }
+  mutations: {
+    SET_ALL_BOARDS: (state: MyStore, payload) => {
+      state.workspace = payload
+    },
+    SET_CURRENT_BOARD: (state: MyStore, board) => {
+      state.currentBoard = board
+    },
+    ADD_CREATED_BOARD: (state: MyStore, board) => {
+      state.workspace.createdBoards.push(board)
+    },
+    UPDATE_CURRENTBOARD_LIST: (state: MyStore, payload) => {
+      state.currentBoard.lists[payload.listId] = payload.data
+    },
+    DELETE_CURRENTBOARD_LIST: (state: MyStore, listId) => {
+      state.currentBoard.lists = state.currentBoard.lists.filter((l) => l._id !== listId)
+    },
+    ADD_CURRENTBOARD_LIST: (state: MyStore, list) => {
+      state.currentBoard.lists.push(list)
+    }
+  },
+  actions: {}
 })
 
-export const useStore = (): MyStore => {
-  return store
-}
+export default store
